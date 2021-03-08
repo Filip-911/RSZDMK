0c84f86615511c37b942e2913ccd1651aa3b8c52
Filip-911 <filiphnovi29@gmail.com>
2021-03-08 13:50:41 +0100

    yet again

--- vjezbe1/zad1/main.c
+++ vjezbe1/zad1/main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
-int main() {
-   printf("Hello, World!");
-   printf("Changes have been made");
+int main() 
+{
+	int a=5;
+	int b=4;
+	int c;
+
+	if(a>b)
+		c=a-b;
+	else
+		c=b-a
+	printf("Result: %d \n", c);
+
    return 0;
 }
