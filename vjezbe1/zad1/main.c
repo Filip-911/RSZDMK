1ca2fa3d187c5f111af4a114d0242d66781d88dd
Filip-911 <filiphnovi29@gmail.com>
2021-03-08 14:05:12 +0100

    Calc func

--- vjezbe1/zad1/main.c
+++ vjezbe1/zad1/main.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
+int Calculation (int a, int b)
+{
+	if(a>b)
+		c=a-b;
+	else
+		c=b-a;
+}
 int main() 
 {
 	int a=5;
 	int b=4;
 	int c;
+	
+	c= Calculation(a,b);
 
-	if(a>b)
-		c=a-b;
-	else
-		c=b-a
 	printf("Result: %d \n", c);
 
    return 0;
