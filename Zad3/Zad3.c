4d6b43fcb01229eaafb43e351bf86dfbdcdb52cc
Filip-911 <filiphnovi29@gmail.com>
2021-05-06 13:37:24 +0200

    radi

--- Zad3/Zad3.c
+++ Zad3/Zad3.c
@@ -10,71 +10,30 @@
 #include <stdio.h>
 #include <util/delay.h>
 #include <avr/pgmspace.h>
+
 typedef uint8_t unt;
 
-char kejs (unt ost);
-
-int main()
-{
+int main() {
 	usartInit(9600);
-	char ost, *broj, hex [5];
+	char broj[16], hex[16];
+	int intBroj;
 
-	while(1)
-	{
+	while (1) {
 		usartPutString_P(PSTR("\r \n"));
 		usartPutString_P(PSTR("Unesite decimalan broj:"));
+		usartPutString_P(PSTR(" \r \n "));
 
-		while(!usartAvailable());
-
-		_delay_ms(100);
+		while (!usartAvailable())
+			;
+		_delay_ms(30);
 		usartGetString(broj);
+		intBroj = atoi(broj);
 
-		for(unt i=0 ; i < 5; i++)
-		{
-			ost = (*broj) % 16;
-			if(ost > 9)
-				hex[4-i]=kejs(ost);
-			else
-				hex[4-i]=ost;
-
-			*broj= (*broj) / 16;
-		}
-		hex[5]='\0';
-
-		usartPutString_P(PSTR("\r \n"));
-		usartPutString_P(PSTR("Unijeli ste vrijednost : 0x"));
+		sprintf(hex, "0x%x", intBroj);
 		usartPutString(hex);
+		usartPutString_P(PSTR(" \n \r "));
+
 	}
 
 	return 0;
-}
-
-char kejs (unt ost)
-{
-	switch (ost)
-	{
-		case 10:
-			return 'A';
-			break;
-
-		case 11:
-			return 'B';
-			break;
-
-		case 12:
-			return 'C';
-			break;
-
-		case 13:
-			return 'D';
-			break;
-
-		case 14:
-			return 'E';
-			break;
-
-		case 15:
-			return 'F';
-			break;
-	}
 }
