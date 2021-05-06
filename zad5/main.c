275885c8ff17ff3e2200c2e889818dc352051dbe
Filip-911 <filiphnovi29@gmail.com>
2021-05-06 15:05:39 +0200

    noup(1)

--- zad5/main.c
+++ zad5/main.c
@@ -13,14 +13,13 @@
 
 typedef uint8_t unt;
 
-double operation (char op, int b1, int b2);
+ void operation (char op, int b1, int b2);
 
 int main()
 {
 	usartInit(9600);
-	char op, broj1[15] , broj2[15], rezs[50];
+	char op, broj1[15] , broj2[15], prob[50];
 	int ibroj1, ibroj2;
-	double rez=0;
 
 	while(1)
 	{
@@ -33,53 +32,79 @@ int main()
 
 		_delay_ms(20);
 
+		char c;
 		unt p=0;
 
-		broj1[p++]=usartGetChar();
+				broj1[p++]=usartGetChar();
 
-		while (broj1[p] <= '9' && broj1[p] >= '0')
-		{
-			broj1[p++]=usartGetChar();
-		}
+				while (broj1[p] <= '9' && broj1[p] >= '0')
+				{
+					broj1[p++]=usartGetChar();
+				}
 
-		broj1[p]='\0';
+				broj1[p]='\0';
+
+
 		ibroj1 = atoi(broj1);
 
+		sprintf(prob, "broj1 : %d", ibroj1);
+		usartPutString(prob);
+
 		op = usartGetChar();
+		usartPutString_P(PSTR("\r \n"));
+
+		sprintf(prob, "op : %c", op);
+
+		usartPutString(prob);
 
 		p =0;
 
-		broj2[p++]=usartGetChar();
-
-		while (broj2[p] <= '9' && broj2[p] >= '0')
+		do
 		{
-			broj2[p++]=usartGetChar();
+			c=usartGetChar();
+			broj2[p++]=c;
 		}
+		while( c >= '9' && c <= '0');
+
 		broj2[p]='\0';
 
+
 		ibroj2 = atoi(broj2);
-
-		rez = operation(op, ibroj1, ibroj2);
-
-		sprintf(rezs, "Rezultat : %lf ", rez);
-
-		usartPutString(rezs);
 		usartPutString_P(PSTR("\r \n"));
+
+		sprintf(prob, "broj2 : %d", ibroj2);
+		usartPutString(prob);
+
+		operation(op, ibroj1, ibroj2);
+
+		usartPutString_P(PSTR("\r \n"));
+
+
 	}
 
 	return 0;
 }
-double operation (char op, int b1, int b2)
+void operation (char op, int b1, int b2)
 {
-	switch(op)
+	char rezs[50];
+	double rez;
+
+	switch (op)
 			{
 			case '*':
-				return b1*b2;
+				rez = (double)b1*(double)b2;
+				break;
 			case '/':
-				return (double)b1/b2;
+				rez = (double)b1/(double)b2;
+				break;
 			case '+':
-				return b1+b2;
+				rez = (double)b1+(double)b2;
+				break;
 			case '-':
-				return b1-b2;
+				rez = (double)b1-(double)b2;
+				break;
 			}
+
+		sprintf(rezs, "Rezultat : %lf ", rez);
+		usartPutString(rezs);
 }
