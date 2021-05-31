f7f983275a81b0d7a33adb56a5aba24a4e906f50
Filip-911 <filiphnovi29@gmail.com>
2021-05-31 18:31:56 +0200

    lift

---
+++ Zad 12.1 - lift/Release/main.d
@@ -0,0 +1,10 @@
+main.o main.o: ../main.c ../../usart/usart.h ../../timer0/timer0.h \
+  ../../pin/pin.h ../../PLS7/PLS7.h
+
+../../usart/usart.h:
+
+../../timer0/timer0.h:
+
+../../pin/pin.h:
+
+../../PLS7/PLS7.h:
